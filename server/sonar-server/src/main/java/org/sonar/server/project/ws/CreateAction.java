fdf2e24e56273e275bedc5375272fbcc5c62cf12
Daniel Schwarz <daniel.schwarz@sonarsource.com>
2017-04-24 16:19:23 +0200

    SONAR-9091 add visibility flag to api/components/create

--- server/sonar-server/src/main/java/org/sonar/server/project/ws/CreateAction.java
+++ server/sonar-server/src/main/java/org/sonar/server/project/ws/CreateAction.java
@@ -35,8 +35,8 @@ import org.sonarqube.ws.client.project.CreateRequest;
 
 import static java.util.Optional.ofNullable;
 import static org.sonar.api.resources.Qualifiers.PROJECT;
-import static org.sonar.server.component.NewComponent.newComponentBuilder;
 import static org.sonar.db.permission.OrganizationPermission.PROVISION_PROJECTS;
+import static org.sonar.server.component.NewComponent.newComponentBuilder;
 import static org.sonar.server.project.ws.ProjectsWsSupport.PARAM_ORGANIZATION;
 import static org.sonar.server.ws.KeyExamples.KEY_PROJECT_EXAMPLE_001;
 import static org.sonar.server.ws.WsUtils.writeProtobuf;
@@ -48,6 +48,7 @@ import static org.sonarqube.ws.client.project.ProjectsWsParameters.PARAM_PROJECT
 public class CreateAction implements ProjectsWsAction {
 
   private static final String DEPRECATED_PARAM_KEY = "key";
+  static final String PARAM_VISIBILITY = "visibility";
 
   private final ProjectsWsSupport support;
   private final DbClient dbClient;
@@ -78,8 +79,7 @@ public class CreateAction implements ProjectsWsAction {
 
     action.setChangelog(
       new Change("6.3", "The response format has been updated and does not contain the database ID anymore"),
-      new Change("6.3", "The 'key' parameter has been renamed 'project'")
-    );
+      new Change("6.3", "The 'key' parameter has been renamed 'project'"));
 
     action.createParam(PARAM_PROJECT)
       .setDescription("Key of the project")
@@ -95,6 +95,14 @@ public class CreateAction implements ProjectsWsAction {
     action.createParam(PARAM_BRANCH)
       .setDescription("SCM Branch of the project. The key of the project will become key:branch, for instance 'SonarQube:branch-5.0'")
       .setExampleValue("branch-5.0");
+
+    action.createParam(PARAM_VISIBILITY)
+      .setDescription("Whether the created project should be visible to everyone, or only specific user/groups.<br/>" +
+        "If no visibility is specified, the default project visibility of the organization will be used.")
+      .setRequired(false)
+      .setInternal(true)
+      .setSince("6.4")
+      .setPossibleValues("private", "public");
 
     support.addOrganizationParam(action);
   }
@@ -116,6 +124,7 @@ public class CreateAction implements ProjectsWsAction {
         .setKey(request.getKey())
         .setName(request.getName())
         .setBranch(request.getBranch())
+        .setPrivate(request.getVisibility().map("private"::equals).orElseGet(() -> dbClient.organizationDao().getNewProjectPrivate(dbSession, organization)))
         .setQualifier(PROJECT)
         .build(),
         userSession.isLoggedIn() ? userSession.getUserId() : null);
@@ -129,6 +138,7 @@ public class CreateAction implements ProjectsWsAction {
       .setKey(request.mandatoryParam(PARAM_PROJECT))
       .setName(request.mandatoryParam(PARAM_NAME))
       .setBranch(request.param(PARAM_BRANCH))
+      .setVisibility(request.param(PARAM_VISIBILITY))
       .build();
   }
 
@@ -137,7 +147,8 @@ public class CreateAction implements ProjectsWsAction {
       .setProject(CreateWsResponse.Project.newBuilder()
         .setKey(componentDto.key())
         .setName(componentDto.name())
-        .setQualifier(componentDto.qualifier()))
+        .setQualifier(componentDto.qualifier())
+        .setVisibility(componentDto.isPrivate() ? "private" : "public"))
       .build();
   }
 
